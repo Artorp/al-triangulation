428e42c478fabc8df28429dc255d2cbbd586e5cd
Artorp <Artorp@users.noreply.github.com>
2020-11-21 18:05:09 +0100

    Remove unused json webpack test rule

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,7 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            },
-            {
-                test: /\.json$/,
-                exclude: /map_data/,
-            },
+            }
         ],
     },
     resolve: {
