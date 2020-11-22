0416f0deabc4615c0fb3a548aaf9bb1d2738e57d
Artorp <Artorp@users.noreply.github.com>
2020-11-22 16:38:52 +0100

    Port generate_navmesh to TypeScript

--- src/generate_navmesh.ts
+++ src/generate_navmesh.ts
@@ -1,19 +1,10 @@
-const { fill_quads_and_remove_doubles } = require("./contours_to_quads");
-const { contours_raycast_edges } = require("./contours_to_quads");
-const { contours_into_horiz_vert_edge_list } = require("./detect_contours");
-const { detect_contours } = require("./detect_contours");
-const { inflate_contours } = require("./inflate_contour");
-const { contours_remove_unused_verts } = require("./detect_contours");
-const { remove_doubles } = require("./remove_doubles");
-const { intersect_and_cut } = require("./map_to_polygons");
-const { xylines_to_edges } = require("./map_to_polygons");
-
-
-/**
- * @typedef {import("./al_jsdoc_types").MapData} MapData
- * @typedef {import("./geometry_types").Point} Point
- * @typedef {import("./geometry_types").Edge} Edge
- * */
+import { Point } from "./geometry_types";
+import { MapData } from "./al_jsdoc_types";
+import { intersect_and_cut, xylines_to_edges } from "./map_to_polygons";
+import { remove_doubles } from "./remove_doubles";
+import { contours_into_horiz_vert_edge_list, contours_remove_unused_verts, detect_contours } from "./detect_contours";
+import { inflate_contours } from "./inflate_contour";
+import { contours_raycast_edges, fill_quads_and_remove_doubles } from "./contours_to_quads";
 
 
 /**
@@ -27,7 +18,10 @@ const { xylines_to_edges } = require("./map_to_polygons");
  *     edge_indices: two-item tuple of indices into vertices that defines edges,
  *     faces: four-item tuple of indices into vertices that defines faces in ccw order
  */
-function generate_navmesh(map_data, from_position) {
+function generate_navmesh(
+    map_data: MapData,
+    from_position: Point
+): { vertices: Point[], edge_indices: [number, number][], faces: [number, number, number, number][] } {
     const [horizontal_edges, vertical_edges] = xylines_to_edges(map_data);
     const edges = intersect_and_cut(horizontal_edges, vertical_edges);
     const { vertices, edge_indices } = remove_doubles(edges);
@@ -43,4 +37,4 @@ function generate_navmesh(map_data, from_position) {
     return { vertices: v2, edge_indices: e2, faces };
 }
 
-module.exports = { generate_navmesh };
+export { generate_navmesh };
